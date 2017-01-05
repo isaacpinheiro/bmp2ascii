3ffb6bba751672feb9868213bd31495327353626
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-01-05 12:24:49 -0200

    Implementando a função 'generate_image_into_file()'.

--- bitmap.c
+++ bitmap.c
@@ -25,6 +25,23 @@ void generate_image(Color *color, unsigned int width, unsigned int height)
 
 void generate_image_into_file(Color *color, unsigned int width, unsigned int height, FILE *out_f)
 {
-    // TODO
+
+    char pixel[10] = { ' ', '.', ',', ':', ';', 'o', 'x', '%', '#', '@' };
+    int i, j;
+
+    for (i=height-1; i>=0; i--) {
+
+        for (j=0; j<width; j++) {
+
+            Color c = color[i*width+j];
+            unsigned char intensity = (unsigned char) (9 - (((c.blue + c.green + c.red)*9)/(255*3)));
+            fprintf(out_f, "%c", pixel[intensity]);
+
+        }
+
+        fprintf(out_f, "\n");
+
+    }
+
 }
 

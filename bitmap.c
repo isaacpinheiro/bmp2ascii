d6bf0fc28619a0eec08109111f51b33c93e7c068
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-01-05 12:15:59 -0200

    Implementando a função 'generate_image()'.

--- bitmap.c
+++ bitmap.c
@@ -7,13 +7,12 @@ void generate_image(Color *color, unsigned int width, unsigned int height)
     char pixel[10] = { ' ', '.', ',', ':', ';', 'o', 'x', '%', '#', '@' };
     int i, j;
 
-    for (i=0; i<height; i++) {
+    for (i=height-1; i>=0; i--) {
 
         for (j=0; j<width; j++) {
 
-            Color c = color[(i*width)+j];
+            Color c = color[i*width+j];
             unsigned char intensity = (unsigned char) (9 - (((c.blue + c.green + c.red)*9)/(255*3)));
-            //printf("%u", ((unsigned char) intensity));
             printf("%c", pixel[intensity]);
 
         }
